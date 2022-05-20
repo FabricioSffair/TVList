72f320c710f7b42d53720b1610ec47d0de0e6428
Fabricio <fabriciosffair@gmail.com>
2022-05-20 16:10:30 -0300

    Initial Commit

---
+++ TVList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TVList
+//
+//  Created by Fabrício Sperotto Sffair on 2022-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TVList/TVListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVListApp.swift
+//  TVList
+//
+//  Created by Fabrício Sperotto Sffair on 2022-05-20.
+//
+
+import SwiftUI
+
+@main
+struct TVListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
